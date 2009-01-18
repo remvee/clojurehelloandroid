7da5fe267ee63215aa28b44cdeb3db6ec280344b
Remco van 't Veer <rwvtveer@xs4all.nl>
2009-01-18 22:04:55 +0100

    smarter clojure compilation script

--- compile.clj
+++ compile.clj
@@ -1,2 +1,16 @@
-(binding [*compile-path* "bin/classes"]
-  (compile 'net.remvee.android.hello.ClojureHelloAndroid))
+(def srcdir (first *command-line-args*))
+(def bindir (second *command-line-args*))
+
+(defn class-name [fname]
+  "Strip srcdir and suffix, and replace file separators by dots."
+  (.. (if (. fname startsWith srcdir) (. fname substring (. srcdir length)) fname)
+      (replaceAll "\\.clj$" "")
+      (replace java.io.File/separatorChar \.)
+      (replaceAll "^\\." "")))
+
+(loop [args (drop 2 *command-line-args*)]
+  (when (first args)
+    (let [cn (class-name (first args))]
+      (println (str "Compiling " cn ".."))
+      (binding [*compile-path* bindir] (compile (symbol cn)))
+      (recur (rest args)))))
